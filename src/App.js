2f4e1ec66b6a2949454c839b307f822d7cdf3a04
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-10-03 01:59:48 +0300

    add Canvas component nesting // remove unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import {Canvas} from "@react-three/fiber";
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Canvas/>
     </div>
   );
 }
